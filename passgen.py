955278514792306f4f96be14423edb329e2fbe21
Eric Zhou <71947872+FullStackGoogler@users.noreply.github.com>
2020-12-16 03:07:33 -0500

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,10 +1,1 @@
-import random
-
-c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@##$%^&*()?"
-len = int(input("pass length: "))
-def randgen(inp):
-    z = ''.join(random.sample(c,inp))
-    print(z)
-
-randgen(len)
-#test
+#uh peepee poopoo
